504b8213e1f3e3ad026471de74422bd3acbeb092
sb121300 <soobin851@kookmin.ac.kr>
2023-03-06 14:53:11 +0900

    [Feat] server & main-page

--- allergy/src/main/java/com/site/allergy/MainController.java
+++ allergy/src/main/java/com/site/allergy/MainController.java
@@ -3,15 +3,11 @@ package com.site.allergy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.RequiredArgsConstructor;
-
 @Controller
-@RequiredArgsConstructor
 public class MainController {
-	
+
 	@GetMapping("/")
 	public String main() {
 		return "main";
 	}
-
 }
